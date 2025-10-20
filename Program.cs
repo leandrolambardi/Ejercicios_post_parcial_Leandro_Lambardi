3668192d85471671acf0cb780cdf73271ccbe995
Leandro Lambardi <leandrolambardi@hotmail.com>
2025-10-20 15:23:24 -0300

    Primer commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Ejercicios_post_parcial_Leandro_Lambardi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
